86847ece82714ec9b81a0256b41aa4e2e79df204
Ahmad <ahmad@ahmd-asdy.local>
2023-01-15 04:59:58 +0330

    challenge 4 have been solved

--- challenge #2/script.js
+++ challenge #2/script.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
 const readline = require("readline");
 const numArr = new Array(20);
+const array4x4 = new Array(4).fill(new Array(4).fill(1));
+
 function findBiggest(arr) {
   let biggest = arr[0][0] * arr[1][1] * arr[2][2] * arr[3][3];
   if (biggest < arr[3][0] * arr[2][1] * arr[1][2] * arr[0][3]) {
     biggest = arr[3][0] * arr[2][1] * arr[1][2] * arr[0][3];
   }
   for (let i = 0; i < 4; i++) {
-    let multiplication = 1;
     for (let j = 0; j < 4; j++) {
+      let multiplication = 1;
       multiplication *= arr[i][j];
       if (multiplication > biggest) {
         biggest = multiplication;
       }
+      let multiplication2 = 1;
       for (let m = 0; m < 4; m++) {
-        multiplication *= arr[j][m];
-        if (multiplication > biggest) {
-          biggest = multiplication;
+        multiplication2 *= arr[j][m];
+        if (multiplication2 > biggest) {
+          biggest = multiplication2;
         }
       }
     }
@@ -35,7 +38,21 @@ void (async () => {
     numArr[lineNumber] = text.split(" ");
     lineNumber++;
   });
-
   await new Promise((res) => rl.once("close", res));
-  console.log(numArr);
+  let finalResult = 0;
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      for (let m = 0; m < 4; m++) {
+        for (let n = 0; n < 4; n++) {
+          array4x4[m][n] = numArr[i + m][j + n];
+        }
+      }
+      let big = findBiggest(array4x4);
+      if (finalResult < big) {
+        finalResult = big;
+      }
+    }
+  }
+  // array16x16.forEach((x) => findBiggest(x));
+  console.log(finalResult);
 })();
