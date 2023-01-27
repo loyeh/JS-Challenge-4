32f22ded16d54ba507b2368b6d0c51c9742aef99
Ahmad <ahmad@ahmd-asdy.local>
2023-01-27 16:37:41 +0330

    bugs fixed

--- challenge #5/script.js
+++ challenge #5/script.js
@@ -8,7 +8,6 @@ class DeliveryTime {
     (this.hour = hour), (this.minute = minute), (this.secound = secound);
   }
 }
-
 function deliveryTimeCalculator(year) {
   // 5 hours            (5×60×60) +
   // 48 minutes            (48×60) +
@@ -16,14 +15,13 @@ function deliveryTimeCalculator(year) {
   //                     --------
   //                        20926
   // difference between to years in seconds
-  let DeliveryTimeSecond = ((year - 1400) * 20926) % (24 * 60 * 60);
+  let DeliveryTimeSecond = ((1400 - year) * 20926) % (24 * 60 * 60);
   let hours = Math.floor((Math.floor(DeliveryTimeSecond / 3600) + 13) % 24);
   let minutes = Math.floor((Math.floor((DeliveryTimeSecond % 3600) / 60) + 7) % 60);
   let seconds = Math.floor(((DeliveryTimeSecond % 60) + 28) % 60);
   const deliveryTime = new DeliveryTime(hours, minutes, seconds);
   return deliveryTime;
 }
-
 function isLeapYear(year) {
   if (deliveryTimeCalculator(year).hour < 12 && deliveryTimeCalculator(year + 1).hour >= 12) {
     return true;
@@ -31,9 +29,8 @@ function isLeapYear(year) {
     return false;
   }
 }
-
 function maxDays(month, year) {
-  if (month < 6) {
+  if (month <= 6) {
     return 31;
   } else if (month < 12) {
     return 30;
@@ -44,36 +41,36 @@ function maxDays(month, year) {
   }
 }
 
-function tommarowCalculator(today) {
+function yesterdayCalculator(today) {
   let year = today.year;
   let month = today.month;
   let dayOfMonth = today.dayOfMonth;
   let dayOfWeek = (today.dayOfWeek + 1) % 7;
-  if (dayOfMonth === maxDays(month, year)) {
-    if (month === 12) {
-      year++;
-      month = 1;
-      dayOfMonth = 1;
+  if (dayOfMonth === 1) {
+    if (month === 1) {
+      year--;
+      month = 12;
+      dayOfMonth = maxDays(year, month);
     } else {
-      dayOfMonth = 1;
-      month++;
+      month--;
+      dayOfMonth = maxDays(year, month);
     }
   } else {
-    dayOfMonth++;
+    dayOfMonth--;
   }
-  const tomarrow = new MyDate(year, month, dayOfMonth, dayOfWeek);
-  return tomarrow;
+  const yesterday = new MyDate(year, month, dayOfMonth, dayOfWeek);
+  return yesterday;
 }
 
 let startDate = new MyDate(1400, 1, 1, 1);
-let counter = 0;
 
-console.log(tommarowCalculator(startDate));
-while (startDate.year < 1500) {
-  startDate = tommarowCalculator(startDate);
-
-  if (startDate.dayOfMonth === 1 && startDate.dayOfWeek === 0) {
-    counter++;
-  }
+while (startDate.year > 1) {
+  startDate = yesterdayCalculator(startDate);
 }
-console.log(startDate, counter);
+while (startDate.month > 1) {
+  startDate = yesterdayCalculator(startDate);
+}
+while (startDate.dayOfMonth > 1) {
+  startDate = yesterdayCalculator(startDate);
+}
+console.log(startDate);
